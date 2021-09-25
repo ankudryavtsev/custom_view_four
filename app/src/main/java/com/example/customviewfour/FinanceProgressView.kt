b2b4e0b25ad842ae91705322dde5797decc01e46
Andrey Kudryavtsev <kudrandrew@gmail.com>
2021-09-25 13:33:41 +0300

    wrap_content

--- app/src/main/java/com/example/customviewfour/FinanceProgressView.kt
+++ app/src/main/java/com/example/customviewfour/FinanceProgressView.kt
@@ -1,10 +1,16 @@
 package com.example.customviewfour
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.annotation.ColorInt
+import kotlin.math.max
+import kotlin.math.min
 
 class FinanceProgressView(
     context: Context,
@@ -33,8 +39,7 @@ class FinanceProgressView(
     }
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    //TODO
-    private val arcRect = RectF(0f, 0f, 500f, 500f)
+    private val arcRect = RectF()
     private val textRect = Rect()
 
     init {
@@ -64,10 +69,49 @@ class FinanceProgressView(
     override fun onDraw(canvas: Canvas?) {
         canvas?.apply {
             translate(strokeWidth / 2, strokeWidth / 2)
-            drawCircle(arcRect.centerX(), arcRect.centerY(), arcRect.centerX(), backgroundPaint)
-            drawArc(arcRect, TOP_START_ANGLE, FULL_CIRCLE_ANGLE / max * progress, false, foregroundPaint)
+            drawArc(arcRect, 0F, FULL_CIRCLE_ANGLE, false, backgroundPaint)
+            drawArc(
+                arcRect,
+                TOP_START_ANGLE,
+                FULL_CIRCLE_ANGLE / max * progress,
+                false,
+                foregroundPaint
+            )
             drawTextProgress()
         }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        Log.d(TAG, "onSizeChanged() called with: w = $w, h = $h, oldw = $oldw, oldh = $oldh")
+        val size = min(w, h) - strokeWidth
+        arcRect.set(
+            paddingLeft.toFloat(),
+            paddingTop.toFloat(),
+            size - paddingRight,
+            size - paddingBottom
+        )
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        Log.d(
+            TAG,
+            "onMeasure() called with: widthMeasureSpec = ${MeasureSpec.toString(widthMeasureSpec)}, " +
+                    "heightMeasureSpec = ${MeasureSpec.toString(heightMeasureSpec)}"
+        )
+        val s = "$max %"
+        textPaint.getTextBounds(s, 0, s.length, textRect)
+
+        val requestedWidth =
+            max(textRect.width() + paddingLeft + paddingRight, suggestedMinimumWidth)
+        val requestedHeight =
+            max(textRect.height() + paddingTop + paddingBottom, suggestedMinimumHeight)
+
+        val requestedSize = (max(requestedWidth, requestedHeight) + strokeWidth * 2).toInt()
+
+        setMeasuredDimension(
+            resolveSizeAndState(requestedSize, widthMeasureSpec, 0),
+            resolveSizeAndState(requestedSize, heightMeasureSpec, 0)
+        )
     }
 
     private fun Canvas.drawTextProgress() {
@@ -96,6 +140,7 @@ class FinanceProgressView(
     }
 
     companion object {
+        private const val TAG = "FinanceProgressView"
         private const val FULL_CIRCLE_ANGLE = 360f
         private const val TOP_START_ANGLE = -90f
     }
